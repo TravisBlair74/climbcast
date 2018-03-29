20a39259101415e2f723bc1daccda5ca3042078f
Travis-Blair <travisblair74@gmail.com>
2018-03-29 15:51:44 -0400

    Roles implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
   has_many :comments
+
+  before_save { self.role ||= :member }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  enum role: [:member, :admin]
 end
